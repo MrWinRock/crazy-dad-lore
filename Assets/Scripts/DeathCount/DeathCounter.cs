97112fa840f394c8b429f1ebd166e69e91f0ffb9
Angkasit Karoonchart <108455691+Marguro@users.noreply.github.com>
2025-08-18 16:45:49 +0700

    refactor : encapsulate DeathCounter class within a namespace and clean up code structure

--- Assets/Scripts/DeathCount/DeathCounter.cs
+++ Assets/Scripts/DeathCount/DeathCounter.cs
@@ -1,40 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathCounter : MonoBehaviour
+namespace DeathCount
 {
-    public static DeathCounter instance;
-    public int deathCount = 0;
-
-    [SerializeField] private int notResetSceneIndex2 = 5; // <== Change this to your reset scene name
-    private int notResetSceneIndex1 = 6; // <== Change this to your not reset scene name
-    private int originalSceneIndex;
-
-    void Awake()
+    public class DeathCounter : MonoBehaviour
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-            originalSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+        public static DeathCounter instance;
+        public int deathCount;
 
-    void Update()
-    {
-        // Only reset if we go to a specific scene (e.g. "MainMenu")
-        if (SceneManager.GetActiveScene().buildIndex == notResetSceneIndex1 || SceneManager.GetActiveScene().buildIndex == notResetSceneIndex2)
+        [SerializeField] private int notResetSceneIndex2 = 5; // <== Change this to your reset scene name
+        private int notResetSceneIndex1 = 6; // <== Change this to your not reset scene name
+        private int originalSceneIndex;
+
+        void Awake()
         {
-            Debug.Log("Not Resetting Death Count");
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+                originalSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
-        else
+
+        void Update()
         {
-            deathCount = 0; // Reset the death count
-            Destroy(gameObject); // this will reset everything next time the original scene is entered
+            // Only reset if we go to a specific scene (e.g. "MainMenu")
+            if (SceneManager.GetActiveScene().buildIndex == notResetSceneIndex1 || SceneManager.GetActiveScene().buildIndex == notResetSceneIndex2)
+            {
+                Debug.Log("Not Resetting Death Count");
+            }
+            else
+            {
+                deathCount = 0; // Reset the death count
+                Destroy(gameObject); // this will reset everything next time the original scene is entered
+            }
         }
     }
 }
